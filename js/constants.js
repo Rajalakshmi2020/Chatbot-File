a9c64f1af5a5de800c11ff7a5c36cd36eba716ae
Rajalakshmi <85389763+Rajalakshmi2020@users.noreply.github.com>
2022-02-04 12:42:16 +0530

    all files

---
+++ js/constants.js
@@ -0,0 +1,86 @@
+const prompts = [
+    ["hi"],
+    ["mild corona symptoms","symptoms"],
+    ["rules for coronavirus"],
+    [ "hey", "hello", "good morning", "good afternoon"],
+    ["how are you", "how is life", "how are things"],
+    ["what are you doing", "what is going on", "what is up"],
+    ["how old are you"],
+    ["who are you", "are you human", "are you bot", "are you human or bot"],
+    ["who created you", "who made you"],
+    [
+      "your name please",
+      "your name",
+      "may i know your name",
+      "what is your name",
+      "what call yourself"
+    ],
+    ["i love you"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+    ["bad", "bored", "tired"],
+    ["help me", "tell me story", "tell me joke"],
+    ["ah", "yes", "ok", "okay", "nice"],
+    ["bye", "good bye", "goodbye", "see you later"],
+    ["what should i eat today"],
+    ["how to wear mask"],
+    ["what", "why", "how", "where", "when"],
+    ["no","not sure","maybe","no thanks"],
+    [""],
+    ["haha","ha","lol","hehe","funny","joke"],
+    ["how to dispose mask"],
+    ["how to protect yourself from coronavirus"]
+  ]
+  
+  // Possible responses, in corresponding order
+  
+  const replies = [
+    ["Hi! how can I help you?"],
+    ["fever, cough, tiredness, loss of taste or smell"],
+    ["wear mask, follow social distance"],
+    ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+    [
+      "Fine... how are you?",
+      "Pretty well, how are you?",
+      "Fantastic, how are you?"
+    ],
+    [
+      "Nothing much",
+      "About to go to sleep",
+      "Can you guess?",
+      "I don't know actually",
+    
+    ],
+    ["I am infinite"],
+    ["I am just a bot", "I am a bot. What are you?"],
+    ["The one true God, JavaScript"],
+    ["I am nameless", "I'm cochat bot, I'm here to help you"],
+    ["I love you too", "Me too"],
+    ["Have you ever felt bad?", "Glad to hear it"],
+    ["Why?", "Why? You shouldn't!", "Try watching TV"],
+    ["What about?", "Once upon a time..."],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    ["Bye", "Goodbye", "See you later"],
+    ["Sushi", "Pizza"],
+    ["Make sure your mask covers your nose, mouth and chin"],
+    ["Great question"],
+    ["That's ok","I understand","What do you want to talk about?"],
+    ["Please say something :("],
+    ["Haha!","Good one!","funny"],
+    ["When you take off your mask, store it in a clean plastic bag, and every day either wash it if it’s a fabric mask or dispose of it in a trash bin if it’s a medical mask"],
+    ["Get Vaccinated and stay up to date on your COVID-19 vaccines"]
+  ]
+  
+  // Random for any other user input
+  
+  const alternative = [
+    "Same",
+    "Go on...",
+    "Bro...",
+    "Try again",
+    "I'm listening...",
+    "I don't understand :/"
+  ]
+  
+  // Whatever else you want :)
+  
+  
